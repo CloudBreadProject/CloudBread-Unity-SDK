4be581a2f379b74dc495ccdf9b77180bd159bc7d
HONGYOONSEOK <yoonseok417@gmail.com>
2017-02-26 01:51:10 +0900

    Make oAuth setting files

---
+++ Assets/CloudBread/Editor/CBAuthEditor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace CloudBread
+{
+	public class CBAuthEditor : EditorWindow 
+	{
+		static CBAuthEditor _instance = null;
+
+		[MenuItem("CloudBread/CB-Authentication")]
+		public static void InitWindow()
+		{
+			if (null == _instance)
+			{
+				_instance = GetWindow<CBAuthEditor>();
+			}
+			else
+			{
+				_instance.Close();
+			}
+		}
+
+		bool _useFacebookAuth = true;
+		bool _useGoogleAuth = false;
+
+		void OnGUI()
+		{
+			GUILayout.BeginVertical ();
+			{
+//				GUILayout.Box("CloudBread Login Service - OAuth 2.0", "IN BigTitle");
+//				GUILayout.Label("");
+				GUILayout.Label ("CloudBread Login Service - OAuth 2.0");
+				GUILayout.Box ("To configure CloudBread Login Service in this Project,\n" +
+					"you can get more information in our Project Sites", "IN Title");
+				GUILayout.Label ("CloudBread 로그인 서비스를 설정하려면, 아래 프로젝트 사이트를 참조하세요.");
+
+				if (GUILayout.Button("OpenSource Project - CloudBread.", GUI.skin.box))
+				{
+					Application.OpenURL("https://github.com/CloudBreadProject/");
+				}
+
+				if (GUILayout.Toggle (_useFacebookAuth, "Facebook Authentication")) {
+					GUILayout.Box ("difjdijsojf");
+					GUILayout.BeginHorizontal ();
+					{
+						GUILayout.Label ("Redirection URL : ");
+						GUILayout.TextField ("", GUILayout.ExpandWidth(true));
+
+					}
+					GUILayout.EndHorizontal ();
+				}
+			}
+			GUILayout.EndVertical ();
+
+		}
+
+
+			
+	}
+
+}
