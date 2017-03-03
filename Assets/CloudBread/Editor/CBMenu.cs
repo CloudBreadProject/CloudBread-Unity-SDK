e1a04a5d98e8af0172a699dcfcd4b25e6804c1ec
HONGYOONSEOK <yoonseok417@gmail.com>
2017-03-03 11:20:19 +0900

    add Toolbar Menu Item

---
+++ Assets/CloudBread/Editor/CBMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace CloudBread
+{
+	public class CBMenu {
+
+
+		#if UNITY_EDITOR
+
+		[MenuItem("CloudBread/Setting", false, 0)]
+		public static void Settings()
+		{
+			Selection.activeObject = CBSetting.instance;
+		}
+
+		[MenuItem("CloudBread/Developers Page", false, 1)]
+		public static void OpenWeb()
+		{
+			string url = "https://github.com/CloudBreadProject";
+			Application.OpenURL(url);
+		}
+
+		[MenuItem("CloudBread/SDK Document", false, 2)]
+		public static void OpenDocs()
+		{
+			string url = "https://github.com/CloudBreadProject/CloudBread/wiki/How-to-use-Unity-SDK-kor";
+			Application.OpenURL(url);
+		}
+
+		[MenuItem("CloudBread/CB-PostMan")]
+		public static void OpenCBPostManWindows()
+		{
+			CBPostman.InitWindow ();
+		}
+
+		/*
+		[MenuItem("CloudBread/CB-Authentication")]
+		public static void OpenAuthenticationSettingsWindows()
+		{
+			CBAuthEditor.InitWindow ();
+		}
+		*/
+
+		#endif
+	}
+}
