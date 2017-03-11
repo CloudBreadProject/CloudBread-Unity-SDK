59a9c926db393c77046266bd76d43a72ccdf6202
HONGYOONSEOK <yoonseok417@gmail.com>
2017-03-11 21:22:12 +0900

    Change Oauth Connector to Services

---
+++ Assets/CloudBread/API/OAuth/BaseOAuth2Services.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CloudBread.OAuth
+{
+	public class BaseOAuth2Services
+	{
+		public BaseOAuth2Services ()
+		{
+			
+		}
+
+		public enum OAuthServices
+		{
+			FaceBook,
+			Google
+		}
+
+		public static BaseOAuth2Services GetServices(OAuthServices services){
+
+			switch (services) {
+
+			case OAuthServices.FaceBook:
+				return new FaceBookServices ();
+				break;
+
+
+			case OAuthServices.Google:
+				break;
+
+			default:
+				break;
+			}
+
+			return new BaseOAuth2Services();
+		}
+
+		public void SessionConfirm(){}
+
+		public void RequestToken()
+		{
+
+		}
+
+		public void RequestUserData()
+		{
+
+		}
+
+	}
+}
+
---
+++ Assets/CloudBread/API/OAuth/FaceBookServices.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace CloudBread.OAuth
+{
+	public class FaceBookServices : BaseOAuth2Services
+	{
+		public FaceBookServices ()
+		{
+		}
+			
+		public void SessionConfirm()
+		{
+
+		}
+
+		public void RequestToken(FacebookData.Post postData_, System.Action<FacebookData.Receive[]> callback_, System.Action<string> errorCallback_ = null)
+		{
+			string url = ".auth/login/facebook";
+			string postData;
+
+			CloudBread.Request(CloudBread.MakeFullUrl(url), JsonUtility.ToJson(postData_), callback_, errorCallback_);
+		}
+
+		public void RequestUserData()
+		{
+
+		}
+
+		public class FacebookData
+		{
+			[Serializable]
+			public struct Post
+			{
+				[SerializeField]
+				public string access_token;
+			}
+
+			[Serializable]
+			public struct Receive
+			{
+				[SerializeField]
+				public string authenticationToken;
+
+				[SerializeField]
+				public User user;
+			}
+
+			[Serializable]
+			public struct User
+			{
+				[SerializeField]
+				public string userId;
+			}
+		}
+	}
+}
+
