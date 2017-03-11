bf1731523471101ff796cbcaed75650125c98c52
HONGYOONSEOK <yoonseok417@gmail.com>
2017-03-11 21:22:35 +0900

    Make OAuth2Setting Files to save Settings

---
+++ Assets/CloudBread/API/OAuth/OAuth2Setting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace CloudBread.OAuth
+{
+	public class OAuth2Setting : ScriptableObject
+	{
+
+		static bool _useFacebook;
+
+		static public string FaceBookRedirectAddress;
+
+
+
+		static bool _useGooglePlay;
+
+		public static string GooglePlayRedirectAddress;
+
+
+
+		static bool _useKaKao;
+
+		public static string KakaoRedirectAddress;
+
+
+		public OAuth2Setting ()
+		{
+		}
+	}
+}
+
