8e4e41740440c97b28a183fdb8d0f54c377c6ddd
HONGYOONSEOK <yoonseok417@gmail.com>
2017-03-27 01:45:55 +0900

    change the role of oauth manager’s

--- Assets/CloudBread/API/OAuth/OAuthManager.cs
+++ Assets/CloudBread/API/OAuth/OAuthManager.cs
@@ -5,32 +5,45 @@ namespace CloudBread.OAuth
 {
     public class OAuthManager 
     {
-        public BaseConnector Connector;
 
-        private bool isUseOAuth = true;
+		public static BaseOAuth2Services OAuthService;
+
+		public enum OAuthServices
+		{
+			facebook,
+			google
+		}
+
+		public static BaseOAuth2Services GetServices(OAuthServices services){
+
+			switch (services) {
+
+			case OAuthServices.facebook:
+				if (OAuth2Setting.UseFacebook) {
+					return new FaceBookServices ();
+				} else {
+					Debug.LogError ("You have to check 'use facebook' checkbox. It's in CloudBread-CBAuthentication, Facebook Tab");
+					return null;
+				}
+
+			case OAuthServices.google:
+				if (OAuth2Setting.UseGooglePlay) {
+					return new GooglePlayServices ();
+				} else {
+					Debug.LogError ("You have to check 'use google play' checkbox. It's in CloudBread-CBAuthentication, Google Tab");
+					return null;
+				}
+
+			default:
+				return null;
+			}
+		}
 
         public OAuthManager()
         {
 
         }
-
-        public OAuthManager(BaseConnector _connector)
-        {
-            Connector = _connector;
-
-        }
-
-
-        public void Clear()
-        {
-
-        }
-
-        
-        public void ReGet()
-        {
-
-        }
+			
     }
 
 }
