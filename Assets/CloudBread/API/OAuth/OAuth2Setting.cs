67477a20a004c97d795e203f745acb2d7999214e
HONGYOONSEOK <yoonseok417@gmail.com>
2017-03-27 00:50:07 +0900

    Update Setting Values for OAuth2 setting

--- Assets/CloudBread/API/OAuth/OAuth2Setting.cs
+++ Assets/CloudBread/API/OAuth/OAuth2Setting.cs
@@ -1,31 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEditor;
 
 namespace CloudBread.OAuth
 {
 	public class OAuth2Setting : ScriptableObject
 	{
+		private const string SettingAssetName = "CBOAuth2Setting";
+		private const string SettingsPath = "CloudBread/Resources";
+		private const string SettingsAssetExtension = ".asset";
 
-		static bool _useFacebook;
+		// Facebook
+		private bool _useFacebook = false;
+		static public bool UseFacebook 
+		{
+			get { return Instance._useFacebook; } 
+			set { Instance._useFacebook = value; } 
+		}
 
-		static public string FaceBookRedirectAddress;
+		private string _facebookRedirectAddress = ".auth/login/facebook";
+		static public string FacebookRedirectAddress 
+		{
+			get { return Instance._facebookRedirectAddress; } 
+			set { Instance._facebookRedirectAddress = value; }
+		}
 
 
 
-		static bool _useGooglePlay;
+		// GoogePlay
+		public bool _useGooglePlay = false;
+		static public bool UseGooglePlay 
+		{
+			get { return instance._useGooglePlay; }
+			set { Instance._useGooglePlay = value; }
+		}
 
-		public static string GooglePlayRedirectAddress;
+		public string _googleRedirectAddress = "aaaa";
+		static public string GoogleRedirectAddress 
+		{
+			get { return instance._googleRedirectAddress; }
+			set { Instance._googleRedirectAddress = value; }
+		}
 
 
-
-		static bool _useKaKao;
-
+		// KaKao
+		private bool _useKaKao = false;
+		public bool UseKaKao
+		{
+			get { return Instance._useKaKao; }
+			set { Instance._useKaKao = value; }
+		}
 		public static string KakaoRedirectAddress;
 
 
-		public OAuth2Setting ()
+
+		private static OAuth2Setting instance = null;
+		public static OAuth2Setting Instance
 		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = Resources.Load(SettingAssetName) as OAuth2Setting;
+					if (instance == null)
+					{
+						// If not found, autocreate the asset object.
+						instance = ScriptableObject.CreateInstance<OAuth2Setting>();
+
+						#if UNITY_EDITOR
+						string properPath = Path.Combine(Application.dataPath, SettingsPath);
+						if (!Directory.Exists(properPath))
+						{
+							Directory.CreateDirectory(properPath);
+						}
+
+						string fullPath = Path.Combine(
+							Path.Combine("Assets", SettingsPath),
+							SettingAssetName + SettingsAssetExtension);
+						AssetDatabase.CreateAsset(instance, fullPath);
+						#endif
+					}
+				}
+				return instance;
+			}
 		}
+
+
 	}
 }
 
