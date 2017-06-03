9ce468b20ef1cc8fe985955b00482d58478b962c
HONGYOONSEOK <yoonseok417@gmail.com>
2017-06-04 02:39:04 +0900

    refactoring code using interfaces

---
+++ Assets/CloudBread/API/OAuth/IOAuthServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CloudBread.OAuth
+{
+	public interface IOAuthServices
+	{
+
+		void RequestToken(string a, Action<BaseOAuth2Services.AzureZumoToken.Receive> callback, Action<string> errorCallback = null);
+
+//		void SessionConfirm();
+//		void RecreateToken();
+	}
+}
+
